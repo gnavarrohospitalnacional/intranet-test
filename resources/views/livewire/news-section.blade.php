bbb5e2cd9787b90d6c01db1c82e233e7d98cf21e
Geral Navarro <gnavarro@hospitalnacional.com>
2026-02-02 03:24:21 -0500

    test deploy

--- resources/views/livewire/news-section.blade.php
+++ resources/views/livewire/news-section.blade.php
@@ -13,7 +13,7 @@
                     {{ $tipoSeleccionado === 'all'
                     ? 'text-green-hn border-green-hn'
                     : 'border-gray-200 hover:border-green-hn' }}">
-                Todas
+                TodasS
             </button>
 
             {{-- Tipos dinámicos --}}
